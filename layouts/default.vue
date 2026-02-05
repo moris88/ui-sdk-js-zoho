ee1d614294567362fa5df03309ee4efe6dad4582
Maurizio Tolomeo <maurizio.tolomeo@outlook.it>
2026-02-05 10:09:12 +0100

    Aggiungi layout predefinito al file di presentazione e crea il layout di default

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useNav } from '@slidev/client'
+
+const { currentPage, total } = useNav()
+</script>
+
+<template>
+  <div
+    class="slidev-layout default relative"
+    :class="{ 'flex flex-col items-center justify-center h-full': currentPage === 1 }"
+  >
+    <slot />
+
+    <!-- Footer solo se non cover -->
+    <div
+      v-if="currentPage !== 1"
+      class="absolute bottom-4 right-6 text-sm opacity-70"
+    >
+      {{ currentPage }} / {{ total }}
+    </div>
+  </div>
+</template>
